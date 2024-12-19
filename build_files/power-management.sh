c3ac42bc65d1307573855c91707561510c65c393
Daniel Xu <danielxu0307@proton.me>
2024-12-18 20:50:18 -0800

    add throttled

--- build_files/power-management.sh
+++ build_files/power-management.sh
@@ -2,11 +2,13 @@
 
 set -euox pipefail
 
-dnf5 remove -y tuned-ppd
+dnf copr enable abn/throttled
+dnf5 remove -y tuned-ppd thermald tuned
 
 dnf5 install -y \
   tlp \
+  throttled \
   tlp-rdw
 
-systemctl enable tlp.service
+systemctl enable tlp.service throttled.service
 systemctl mask systemd-rfkill.service systemd-rfkill.socket
